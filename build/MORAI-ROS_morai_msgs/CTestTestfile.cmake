d1462d92cc00f8499e75887cdadba22f639230c8
woooong99 <139222316+woooong99@users.noreply.github.com>
2024-01-18 17:41:52 +0900

    delete

--- build/MORAI-ROS_morai_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kss/kmu_ws/src/MORAI-ROS_morai_msgs
-# Build directory: /home/kss/kmu_ws/build/MORAI-ROS_morai_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
